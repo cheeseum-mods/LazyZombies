cb7cb61dee5074427ebef33f6e0e9c21171defbc
Cheeseum <z.cheeseum@gmail.com>
2014-10-08 23:21:57 -0700

    redo everything

--- README.md
+++
@@ -1,9 +0,0 @@
-# Lazy Zombies
-This little coremod just makes zombies not bang on doors. They will still chase villagers and moan while waiting outside houses.
-
-## Building
-Good luck, maybe I'll make it friendlier later.
-
-## Installation
-press button on forge installer
-mod jar goes in mods folder
---
+++ src/main/java/com/cheeseum/lazyzombies/LazyZombies.java
@@ -0,0 +1,49 @@
+package com.cheeseum.lazyzombies;
+
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.pathfinding.PathNavigate;
+import net.minecraft.tileentity.TileEntityMobSpawner;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = LazyZombies.MODID, version = LazyZombies.VERSION)
+public class LazyZombies
+{
+    public static final String MODID = "lazyzombies";
+    public static final String VERSION = "1.0";
+   
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	MinecraftForge.EVENT_BUS.register(this);
+    }
+    
+    @SubscribeEvent
+    public void entityJoin(EntityJoinWorldEvent event)
+    {
+    	if (event.entity instanceof EntityZombie) {
+    		EntityZombie zombie = (EntityZombie)event.entity;
+    		
+    		// never break doors
+    		PathNavigate nav = zombie.getNavigator();
+    		if (nav != null)
+    			nav.setBreakDoors(false);
+    		
+    		// nearby villagers only
+    		if (zombie.targetTasks.taskEntries.size() > 2) {
+	    		zombie.targetTasks.taskEntries.remove(2);
+		        zombie.targetTasks.addTask(2, new EntityAINearestAttackableTarget(zombie, EntityVillager.class, 0, true));
+    		}
+    	}
+    }
+    
+}
